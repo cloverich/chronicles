52efaedfdd3786bb7aa2274c73a970f112346e70
Christopher Loverich <cloverich@gmail.com>
2021-10-15 07:43:01 -0500

    image fix. broken test. errors
    - add fix for images nested in paragraph tags
    - add a test for the parse step, even though i cannot get it to run :|
    - add a top-level ErrorBoundary

---
+++ src/markdown/index.test.ts
@@ -0,0 +1,170 @@
+import { describe, it } from "mocha";
+import { expect } from "chai";
+import { stringToSlate } from "./";
+
+// NOTE: I Never actually ran this because I can't get mocha to work...
+// TypeError [ERR_UNKNOWN_FILE_EXTENSION]: Unknown file extension ".ts" for /Users/cloverich/code/pragma/src/markdown/index.test.ts
+// ...although I have ts-node/register, esm, etc. Need to research...
+// ...this ecosystem is quickly killing me
+describe("Markdown to Slate conversion", function () {
+  it("unnests image tags", function () {
+    const input = `
+I have a few ideas to record:
+
+-   My first idea 
+-   My second idea
+-   My third idea is \_nested\_
+
+    -   It seems to work!
+
+
+
+Now lets add an image: 
+
+![](ckure3z1b00003u65tfr1m2ki..png)
+
+This works! _Seriously_, no **complaints**.
+
+`;
+
+    const slateDOM = [
+      {
+        type: "p",
+        children: [
+          {
+            text: "I have a few ideas to record:",
+          },
+        ],
+      },
+      {
+        type: "ul",
+        children: [
+          {
+            type: "li",
+            children: [
+              {
+                type: "lic",
+                children: [
+                  {
+                    text: "My first idea ",
+                  },
+                ],
+              },
+            ],
+            checked: null,
+            spread: false,
+          },
+          {
+            type: "li",
+            children: [
+              {
+                type: "lic",
+                children: [
+                  {
+                    text: "My second idea",
+                  },
+                ],
+              },
+            ],
+            checked: null,
+            spread: false,
+          },
+          {
+            type: "li",
+            children: [
+              {
+                type: "lic",
+                children: [
+                  {
+                    text: "My third idea is ",
+                  },
+                  {
+                    text: "_",
+                  },
+                  {
+                    text: "nested",
+                  },
+                  {
+                    text: "_",
+                  },
+                ],
+              },
+              {
+                type: "ul",
+                children: [
+                  {
+                    type: "li",
+                    children: [
+                      {
+                        type: "lic",
+                        children: [
+                          {
+                            text: "It seems to work!",
+                          },
+                        ],
+                      },
+                    ],
+                    checked: null,
+                    spread: false,
+                  },
+                ],
+                ordered: false,
+                start: null,
+                spread: false,
+              },
+            ],
+            checked: null,
+            spread: true,
+          },
+        ],
+        ordered: false,
+        start: null,
+        spread: false,
+      },
+      {
+        type: "p",
+        children: [
+          {
+            text: "Now lets add an image: ",
+          },
+        ],
+      },
+      {
+        type: "img",
+        url: "chronicles://ckure3z1b00003u65tfr1m2ki..png",
+        title: null,
+        alt: null,
+        children: [
+          {
+            text: "",
+          },
+        ],
+      },
+      {
+        type: "p",
+        children: [
+          {
+            text: "This works! ",
+          },
+          {
+            italic: true,
+            text: "Seriously",
+          },
+          {
+            text: ", no ",
+          },
+          {
+            bold: true,
+            text: "complaints",
+          },
+          {
+            text: ".",
+          },
+        ],
+      },
+    ];
+
+    const parsed = stringToSlate(input);
+    expect(parsed).to.deep.equal(slateDOM);
+  });
+});
--- src/markdown/index.ts
+++ src/markdown/index.ts
@@ -13,6 +13,8 @@ import { Node as SNode } from "slate";
 
 export * from "ts-mdast";
 
+import { unnestImages } from "./unnestImages";
+
 // On remark versions > 12 I think they moved to micromark, which lost the
 // gfm option and instead requires this package
 import remarkGfm from "remark-gfm";
@@ -74,18 +76,25 @@ const slateToStringProcessor = unified()
   .use(slateToRemark)
   .use(remarkStringify);
 
-// Intermediate markdown parser, exported here so I could store the intermediate
-// mdast state prior to parsing to Slate DOM for debugging purposes
-const stringToMdast = unified().use(remarkParse);
-const stringToSlateProcessor = stringToMdast.use(remarkToSlate);
+const stringToSlateProcessor = unified()
+  .use(remarkParse)
+  .use(unnestImages)
+  .use(remarkToSlate);
 
 export function stringToSlate(text: string) {
   // remarkToSlate must use a newer version, where file.result exists
   // file.contents also exists here... maybe the compilers are overlapping since
   // they modify in place? shrug
   // https://github.com/unifiedjs/unified/releases/tag/9.0.0
-  const output = stringToSlateProcessor.processSync(text);
-  return (output as any).result;
+  // const output = stringToSlateProcessor.processSync(text);
+  const mdast = stringToSlateProcessor.parse(text);
+  console.log("mdast", JSON.stringify(mdast, null, 2));
+  const transformed = stringToSlateProcessor.runSync(mdast);
+  console.log("transformed", JSON.stringify(transformed, null, 2));
+  const output = stringToSlateProcessor.stringify(transformed);
+  console.log("output", JSON.stringify(output, null, 2));
+  // return (output as any).result;
+  return output as any;
 }
 
 export function slateToString(nodes: SNode[]) {
