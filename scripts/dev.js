01371d902caa33b17989b49e4a082210c3b3b5f0
chris <1010084+cloverich@users.noreply.github.com>
2024-01-29 11:00:35 -0800

    upgrade esbuild (#149)
    - upgrades esbuild to latest version, as part of core dependencies review
    - incorporate lint into build step, and prefer yarn run build to calling ./build.sh directly
    - update README and some documentation (comments)

--- scripts/dev.js
+++ scripts/dev.js
@@ -3,90 +3,49 @@ const cp = require("child_process");
 const electron = require("electron");
 const lodash = require("lodash");
 
-// After successful rebuild, log results
-function afterRebuild(name, cb) {
-  return (error, result) => {
-    if (error) {
-      console.error(`${name} bundle completed with error`, error);
-    } else {
-      console.log(`${name} bundle completed`);
-      restartElectron();
-    }
+// Builds and watches the application for development
+// Bundles the renderer, preload, and main processes, and
+// starts Electron, then watches for changes
+
+const startTracker = new Set();
+let didStart = false;
+
+// esbuild callbacks work as plugins; this one handles (re)starting the electron
+// process after the application bundles are built
+function startElectronPlugin(name) {
+  return {
+    name: "(Re)start Electron",
+    setup(build) {
+      build.onEnd((result) => {
+        // NOTE: This will not handle type checking; see type checking call below
+        if (result.errors.length) {
+          console.error(`${name} bundle completed with errors`, result.errors);
+        } else {
+          console.log(`${name} bundle completed`);
+          startTracker.add(name);
+        }
+
+        if (startTracker.size !== 3) return;
+
+        if (didStart) {
+          restartElectron();
+        } else {
+          didStart = true;
+          startElectron();
+        }
+      });
+    },
   };
 }
-
-// After successful build, log results
-function afterBuild(name) {
-  return ({ errors, warnings, ...rest }) => {
-    if (errors.length) {
-      console.error(`${name} bundle completed with errors`, errors);
-    } else if (warnings.length) {
-      console.warn(`${name} bundle completed with warnings`, warnings);
-    } else {
-      console.log(`${name} bundle completed`);
-    }
-
-    // note: until I see errors or warnings, unsure if I should have any special behavior...
-    startElectron();
-  };
-}
-
-// build renderer bundle
-esbuild
-  .build({
-    entryPoints: ["src/index.tsx"],
-    outfile: "src/renderer.bundle.js",
-    bundle: true,
-    platform: "browser",
-    watch: {
-      onRebuild: afterRebuild("renderer"),
-    },
-  })
-  .then(afterBuild("renderer"), console.error);
-
-// build preload bundle
-esbuild
-  .build({
-    entryPoints: ["src/preload/index.ts"],
-    outfile: "src/preload.bundle.js",
-    bundle: true,
-    platform: "node",
-    external: ["knex", "electron", "electron-store", "better-sqlite3"],
-    watch: {
-      onRebuild: afterRebuild("preload"),
-    },
-  })
-  .then(afterBuild("preload"), console.error);
-
-// build electron main bundle
-esbuild
-  .build({
-    entryPoints: ["src/electron/index.js"],
-    outfile: "src/main.bundle.js",
-    bundle: true,
-    platform: "node",
-    external: ["electron", "electron-store", "better-sqlite3"],
-    watch: {
-      onRebuild: afterRebuild("main"),
-    },
-  })
-  .then(afterBuild("main"), console.error);
 
 // For holding the spawned Electron main process, so it can
 // be .kill()'ed on re-start
 let eprocess;
 
-// Track build completions, see below
-let startCounter = 0;
-
-// Start the electron main process
+// Start the electron main process (first time)
 function startElectron() {
-  // Naive way to wait for all three bundles before starting the first time, since
-  // main bundle completes quickest
-  startCounter++;
-  if (startCounter < 3) return;
-
   console.log("starting electron");
+  checkTypes();
   eprocess = cp.spawn(`${electron}`, ["src/main.bundle.js"], {
     stdio: "inherit",
   });
@@ -98,10 +57,17 @@ function startElectron() {
 }
 
 // Re-start the main process
+// todo: This works, but is heavy-handed since it fully re-starts the process each time.
+// Ideally renderer code would hot-reload, or merely refresh and load from a dev-server
+// instead or restarting electron
 const restartElectron = lodash.debounce(function startElectron() {
   if (eprocess) eprocess.kill("SIGTERM");
 
-  console.log("starting electron");
+  // todo: This was a quick hack to get type errors to show up in console. Re-write this as a
+  // plugin that checks the relevant types (renderer, preload) and fails the build (and, ideally,
+  // is incremental or something, rather than a fresh sub-process)
+  checkTypes();
+  console.log("restarting electron");
   eprocess = cp.spawn(`${electron}`, ["src/main.bundle.js"], {
     stdio: "inherit",
   });
@@ -111,3 +77,56 @@ const restartElectron = lodash.debounce(function startElectron() {
     process.exit(1);
   });
 }, 200);
+
+const checkTypes = lodash.debounce(function checkTypes() {
+  const typesProcess = cp.spawn("yarn", ["tsc", "--noEmit"], {
+    stdio: "inherit",
+  });
+
+  typesProcess.on("error", (error) => {
+    console.error("types error", error, error.message);
+    process.exit(1);
+  });
+}, 200);
+
+async function watchRenderer() {
+  const ctxRenderer = await esbuild.context({
+    entryPoints: ["src/index.tsx"],
+    outfile: "src/renderer.bundle.js",
+    bundle: true,
+    platform: "browser",
+    plugins: [startElectronPlugin("renderer")],
+  });
+
+  await ctxRenderer.watch();
+}
+
+async function watchPreload() {
+  const ctxPreload = await esbuild.context({
+    entryPoints: ["src/preload/index.ts"],
+    outfile: "src/preload.bundle.js",
+    bundle: true,
+    platform: "node",
+    external: ["knex", "electron", "electron-store", "better-sqlite3"],
+    plugins: [startElectronPlugin("preload")],
+  });
+
+  await ctxPreload.watch();
+}
+
+async function watchMain() {
+  const ctxMain = await esbuild.context({
+    entryPoints: ["src/electron/index.js"],
+    outfile: "src/main.bundle.js",
+    bundle: true,
+    platform: "node",
+    external: ["electron", "electron-store", "better-sqlite3"],
+    plugins: [startElectronPlugin("main")],
+  });
+
+  await ctxMain.watch();
+}
+
+watchMain();
+watchPreload();
+watchRenderer();
