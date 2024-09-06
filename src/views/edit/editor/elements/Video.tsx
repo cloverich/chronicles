8efd03bdc49865d1035052a456a677906a8db326
Christopher Loverich <1010084+cloverich@users.noreply.github.com>
2024-09-05 21:26:19 -0700

    add image size constraints
    - adds a max height constriant for images
    - remove resize handler (does not persist to markdown) and caption (was broken); to address in a future version

--- src/views/edit/editor/elements/Video.tsx
+++ src/views/edit/editor/elements/Video.tsx
@@ -1,7 +1,6 @@
 import { PlateElement, PlateRenderElementProps } from "@udecode/plate-common";
 import React from "react";
 import { ELEMENT_VIDEO } from "../plugins/createVideoPlugin";
-import { Caption, CaptionTextarea } from "./Caption";
 import { MediaPopover } from "./MediaPopover";
 
 /**
@@ -16,24 +15,18 @@ export const VideoElement = ({
   return (
     <MediaPopover pluginKey={ELEMENT_VIDEO}>
       <PlateElement asChild className={className} {...props}>
-        <figure className="group relative m-0" contentEditable={false}>
-          <video
-            src={props.element.url as any}
-            controls
-            style={{
-              display: "block",
-              maxWidth: "80%",
-              maxHeight: "20em",
-              margin: "auto", // center
-            }}
-          >
-            Unable to load video.
-          </video>
-          <Caption align="center">
-            <CaptionTextarea placeholder="Write a caption..." />
-          </Caption>
-          {children}
-        </figure>
+        <video
+          src={props.element.url as any}
+          controls
+          style={{
+            display: "block",
+            maxWidth: "80%",
+            maxHeight: "20em",
+            margin: "auto", // center
+          }}
+        >
+          Unable to load video.
+        </video>
       </PlateElement>
     </MediaPopover>
   );
