73d4306ef70c7efdc984b75e52d413cf3859d653
Christopher Loverich <1010084+cloverich@users.noreply.github.com>
2024-09-12 20:55:45 -0700

    clean-up sidebar / more Tags usage
    - clean-up sidebar styling (consistent headings, real tags, remove evergreen components)
    - more abstraction around Tag component (variants, etc)
    - abstract Collapse
    - replace Badge with Tag in documents index view

---
+++ src/components/Collapse.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import { Icons } from "./icons";
+
+type CollapseProps = React.PropsWithChildren<{
+  defaultOpen?: boolean;
+  heading: string;
+}>;
+
+/**
+ * Collapse component that can be toggled open and closed.
+ */
+export function Collapse({ heading, defaultOpen, children }: CollapseProps) {
+  const [isOpen, setIsOpen] = React.useState(
+    defaultOpen == null ? false : defaultOpen,
+  );
+
+  const Icon = isOpen ? Icons.chevronDown : Icons.chevronRight;
+
+  return (
+    <div>
+      <div
+        className="text-md mb-2 flex cursor-pointer items-center font-medium tracking-tight"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {heading}
+        <Icon size={18} />
+      </div>
+      {isOpen && children}
+    </div>
+  );
+}
