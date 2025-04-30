2b48a04b5c331209cc5a3c1e10cd8431b80112bf
Christopher Loverich <1010084+cloverich@users.noreply.github.com>
2025-04-29 21:44:23 -0700

    fix: image drop normalization
    - add a (hacky) normalization routine to move images dropped onto list items to the top level
    
    Resolves an issue where dragging an image onto a list item (and probably other non-image elements) results in it being deleted, deleting the list item, or doing other wonky things

---
+++ src/views/edit/editor/plugins/createNormalizeImagesPlugin.tsx
@@ -0,0 +1,35 @@
+import { PlatePlugin } from "@udecode/plate-core";
+import { ELEMENT_IMAGE } from "@udecode/plate-media";
+import { isElement } from "@udecode/slate";
+import { Editor, Transforms } from "slate";
+
+/**
+ * Ensure a dropped image is always at the top level of the document.
+ *
+ * @returns
+ */
+export const createNormalizeImagesPlugin = (): PlatePlugin => ({
+  key: "top-level-image-normalizer",
+  withOverrides: (editor) => {
+    // NOTE: As of now Chronicle sonly supports images as top-level elements, or as part of ImageGroupElements,
+    // not as children of others. I don't yet understand enough Slate / Plate to grasp the idiomatic approach here,
+    // this is my best guess a a solution, after discovering that dropping an image onto a node with a list
+    // would either rash the editor (earlier versions) or delete the image and the list item (as of writing).
+    const { normalizeNode } = editor;
+
+    editor.normalizeNode = ([node, path]) => {
+      if (isElement(node) && node.type === ELEMENT_IMAGE && path.length > 1) {
+        // Move to root
+        Transforms.removeNodes(editor as Editor, { at: path });
+        Transforms.insertNodes(editor as Editor, node, {
+          at: [editor.children.length],
+        });
+        return;
+      }
+
+      normalizeNode([node, path]);
+    };
+
+    return editor;
+  },
+});
