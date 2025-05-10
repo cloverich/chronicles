75bc39edbd003ebdc877af7409da88fffe4f2664
Christopher Loverich <1010084+cloverich@users.noreply.github.com>
2025-05-10 08:39:31 -0700

    fix: duplicate images on paste
    - fix a bug where dropping a selection of images (or videos, files) would result in an exponential number of duplicates being created
    - refactor file handlers and uploadImage as bytes (ArrayBuffer) rather than base64
    
    On investigation I found I was passing the entire data transfer object on each iteration of files while processing. Unfortunately the routine could not simply modify (pull out) the files it could handle, or mark a subset of them as processed before delegating, nor create a new data transfer object with the subset it intended to pass on (delegate) - for browser security reason. Instead I unified all media handling under one hood; this should also make it easier to later handle uploads in the background with progress indicators to user and better error handling. Will defer to a future improvement.

--- src/views/edit/editor/plugins/createVideoPlugin.tsx
+++ src/views/edit/editor/plugins/createVideoPlugin.tsx
@@ -1,10 +1,4 @@
-import {
-  PlateEditor,
-  WithPlatePlugin,
-  createPluginFactory,
-  insertNode,
-} from "@udecode/plate-common";
-import { isVideoUrl } from "../../../../hooks/images";
+import { createPluginFactory } from "@udecode/plate-common";
 // Ideally this is injected
 import { IClient } from "../../../../hooks/useClient";
 
@@ -13,63 +7,14 @@ const client: IClient = (window as any).chronicles.createClient();
 export const ELEMENT_VIDEO = "video";
 
 /**
- * createVideoPlugin handles copying drag and dropped video files, then injecting
- * a video element with a reference to its copied url.
+ * Supports the video element.
+ *
+ * NOTE: Used to handle video upload prior to unifying in createMediaPlugin; I think this plugin
+ * is still necessary for the video element to work.
  */
 export const createVideoPlugin = createPluginFactory({
   key: ELEMENT_VIDEO,
-
   // https://docs.slatejs.org/concepts/02-nodes
   isElement: true,
-
-  // Means: This element is not editable.
   isVoid: true,
-
-  withOverrides: (editor: PlateEditor, _: WithPlatePlugin) => {
-    // store reference to original insertData function; we'll call it later
-    const insertData = editor.insertData;
-
-    // override insertData function to handle video files
-    editor.insertData = (dataTransfer: DataTransfer) => {
-      const text = dataTransfer.getData("text/plain");
-      const { files } = dataTransfer;
-
-      // note: !text copied from createImagePlugin; I'm unsure if it's necessary.
-      if (!text && files && files.length > 0) {
-        for (const file of files) {
-          const [mime] = file.type.split("/");
-          const extension = (file.name.split(".").pop() || "").toLowerCase();
-
-          if (mime == "video") {
-            // The slate-mdast parser / serializer relies on a whitelist of
-            // extensions to parse and serialize the video element correctly.
-            if (!isVideoUrl(file.name)) {
-              console.error("Unsupported video extension:", extension);
-              continue;
-            }
-
-            client.files.uploadFile(file).then((json: any) => {
-              // TODO: The structure of the inserted node follows that of Image nodes. Slate / Plate have a
-              // schema somewhere (normalization?), and deviating from it here results in SILENT FAILURE.
-              // Figure out the proper place to document and type valid nodes; discovering this behavior
-              // was hell.
-              insertNode(editor, {
-                type: ELEMENT_VIDEO,
-                url: `chronicles://../attachments/${json.filename}`,
-                children: [{ text: "" }],
-              });
-            });
-          } else {
-            // If it's not a video, delegate to the next plugin.
-            insertData(dataTransfer);
-          }
-        }
-      } else {
-        // If it's not a file, delegate to the next plugin.
-        insertData(dataTransfer);
-      }
-    };
-
-    return editor;
-  },
 });
