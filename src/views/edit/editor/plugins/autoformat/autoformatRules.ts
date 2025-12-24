e257c7d256df1ef8de51c718db1e0a21869c395d
Christopher Loverich <1010084+cloverich@users.noreply.github.com>
2025-12-23 22:05:40 -0800

    fix: allow hash in code block
    - fix autoformat eating characters within code blocks

--- src/views/edit/editor/plugins/autoformat/autoformatRules.ts
+++ src/views/edit/editor/plugins/autoformat/autoformatRules.ts
@@ -4,6 +4,7 @@ import { insertEmptyCodeBlock } from "@udecode/plate-code-block";
 import {
   ELEMENT_BLOCKQUOTE,
   ELEMENT_CODE_BLOCK,
+  ELEMENT_CODE_LINE,
   ELEMENT_H1,
   ELEMENT_H2,
   ELEMENT_H3,
@@ -24,24 +25,38 @@ import { formatList, preFormat } from "./autoformatUtils";
 // https://platejs.org/docs/autoformat#autoformatrules
 // Consolidated and simplified the rules configuration into this single file.
 
+/**
+ * Query function to disable autoformat rules inside code blocks.
+ * Returns false (skip rule) when selection is inside a code_block or code_line.
+ * @see https://github.com/cloverich/chronicles/issues/332
+ */
+const notInCodeBlock = (editor: any): boolean => {
+  return !editor.api.some({
+    match: { type: [ELEMENT_CODE_BLOCK, ELEMENT_CODE_LINE] },
+  });
+};
+
 export const autoformatRules: AutoformatRule[] = [
   {
     mode: "block",
     type: ELEMENT_H1,
     match: "# ",
     preFormat,
+    query: notInCodeBlock,
   },
   {
     mode: "block",
     type: ELEMENT_H2,
     match: "## ",
     preFormat,
+    query: notInCodeBlock,
   },
   {
     mode: "block",
     type: ELEMENT_H3,
     match: "### ",
     preFormat,
+    query: notInCodeBlock,
   },
 
   // I don't intend to support H4-H6 at this time
@@ -68,6 +83,7 @@ export const autoformatRules: AutoformatRule[] = [
     type: ELEMENT_BLOCKQUOTE,
     match: "> ",
     preFormat,
+    query: notInCodeBlock,
   },
   {
     mode: "block",
@@ -75,6 +91,7 @@ export const autoformatRules: AutoformatRule[] = [
     match: ["* ", "- "],
     preFormat,
     format: (editor) => formatList(editor, ELEMENT_UL),
+    query: notInCodeBlock,
   },
   {
     mode: "block",
@@ -82,19 +99,24 @@ export const autoformatRules: AutoformatRule[] = [
     match: ["1. ", "1) "],
     preFormat,
     format: (editor) => formatList(editor, ELEMENT_OL),
+    query: notInCodeBlock,
   },
   // Inline code: single backtick wrapping text like `code`
   // Must be before code block rule so it's checked first
+  // Note: We intentionally do NOT add notInCodeBlock here since backticks
+  // in code blocks are just regular characters and won't trigger mark formatting
   {
     mode: "mark",
     type: MARK_CODE,
     match: "`",
+    query: notInCodeBlock,
   },
   // Code block: triple backticks followed by space at start of line
+  // Note: This rule only applies outside code blocks (you can't nest code blocks)
   {
     mode: "block",
     type: ELEMENT_CODE_BLOCK,
-    match: "``` ",
+    match: "```",
     preFormat,
     format: (editor) => {
       insertEmptyCodeBlock(editor as any, {
@@ -102,51 +124,61 @@ export const autoformatRules: AutoformatRule[] = [
         insertNodesOptions: { select: true },
       });
     },
+    query: notInCodeBlock,
   },
   {
     mode: "mark",
     type: [MARK_BOLD, MARK_ITALIC],
     match: "***",
+    query: notInCodeBlock,
   },
   {
     mode: "mark",
     type: [MARK_UNDERLINE, MARK_ITALIC],
     match: "__*",
+    query: notInCodeBlock,
   },
   {
     mode: "mark",
     type: [MARK_UNDERLINE, MARK_BOLD],
     match: "__**",
+    query: notInCodeBlock,
   },
   {
     mode: "mark",
     type: [MARK_UNDERLINE, MARK_BOLD, MARK_ITALIC],
     match: "___***",
+    query: notInCodeBlock,
   },
   {
     mode: "mark",
     type: MARK_BOLD,
     match: "**",
+    query: notInCodeBlock,
   },
   {
     mode: "mark",
     type: MARK_UNDERLINE,
     match: "__",
+    query: notInCodeBlock,
   },
   {
     mode: "mark",
     type: MARK_ITALIC,
     match: "*",
+    query: notInCodeBlock,
   },
   {
     mode: "mark",
     type: MARK_ITALIC,
     match: "_",
+    query: notInCodeBlock,
   },
   {
     mode: "mark",
     type: MARK_STRIKETHROUGH,
     match: "~~",
+    query: notInCodeBlock,
   },
   // I haven't set these up yet
   // {
